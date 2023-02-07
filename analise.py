b9af6e69f01693181dff798ba4eab988dc0b707d
Igorsmh <igorsmh@hotmail.com>
2023-02-07 13:10:53 -0300

    otimização_das_tabelas

--- analise.py
+++ analise.py
@@ -8,40 +8,30 @@ df = bd.read_table(dataset_id='br_mme_consumo_energia_eletrica',
 
 print(df.info())
 
+
 # Evolução do consumo de energia elétrica no Brasil nos últimos 10 anos
 
-groupTotAno = df.groupby(df['ano']).sum()
-groupTotAno = groupTotAno.reset_index()
-groupTotAno.drop(axis=1, columns=['mes', 'numero_consumidores'], inplace=True)
-
-# consumo em Gigawatt-hora e inteiro para facilitar
-groupTotAno['consumo'] = (groupTotAno['consumo']/1000).astype('int64')
+group_consumo_ano = df.groupby(df['ano'])['consumo'].agg([sum, 'mean'])
+group_consumo_ano = group_consumo_ano.reset_index()
 
 
-plt.title("Gráfico do consumo de energia por ano")
+plt.title("Gráfico do consumo de energia consumida por ano")
 plt.xlabel("Ano")
-plt.ylabel("Consumo em GWh")
-plt.plot(groupTotAno['ano'],groupTotAno['consumo'])
-plt.scatter(data=groupTotAno, x="ano", y="consumo")
+plt.ylabel("Consumo em MWh")
+plt.plot(group_consumo_ano['ano'], group_consumo_ano['sum'])
+plt.scatter(data=group_consumo_ano, x="ano", y="sum")
 plt.show()
 
-
-# Média de consumo por anos
-groupMedianAno = df.groupby(df['ano']).mean()
-groupMedianAno = groupMedianAno.reset_index()
-groupMedianAno.drop(axis=1, columns=['mes', 'numero_consumidores'], inplace=True)
-
-# consumo em Gigawatt-hora e inteiro para facilitar
-
+#
 plt.title("Gráfico da média de consumo a cada ano")
 plt.xlabel("Ano")
-plt.ylabel("Consumo Médio")
-plt.bar(groupMedianAno['ano'],groupMedianAno['consumo'])
+plt.ylabel("Consumo Médio em MWh")
+plt.bar(group_consumo_ano['ano'], group_consumo_ano['mean'])
 plt.show()
 
 
-print(df.groupby(df['ano'])['consumo'].agg([sum,'mean']))
 
+# verificar alteração no consumo por quantidade de consumidores
 
 # Bucketsize
 
